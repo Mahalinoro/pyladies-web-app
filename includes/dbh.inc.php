ee2e2eb048d7526994f6e2586aebda7b5eaf4ff4
Mahaly <m.razafiman@alustudent.com>
2020-05-14 09:22:49 +0300

    updated database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $dBUsername = "root";
-    $dBPassword = "";
-    $dBName = "pyladiesTnr";
+    $servername = "us-cdbr-east-06.cleardb.net";
+    $dBUsername = "bcda1914fdb60e";
+    $dBPassword = "4e035ab1";
+    $dBName = "heroku_2e6937e4e5ae220";
 
     $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
@@ -10,3 +10,4 @@
         die("connection failed: ".mysqli_connect_error());
     }
 ?>
+
